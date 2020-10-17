bcdd892dbb9a169e0ae0a27c6d44be659887fdde
Fernando Rios <feriosch98@gmail.com>
2020-10-16 21:14:46 -0500

    Draggable Menu

---
+++ src/stateEnum.js
@@ -0,0 +1,7 @@
+export default {
+  SALIDA_DE_PLANTA: 'salida_de_planta',
+  EN_LOCAL_DELIVERY_CENTER: 'ldc',
+  EN_PROCESO_DE_ENTREGA: 'proceso_de_entrega',
+  ENTREGA_COMPLETA: 'entrega_completada',
+  ENTREGA_FALLIDA: 'entrega_fallida',
+}
